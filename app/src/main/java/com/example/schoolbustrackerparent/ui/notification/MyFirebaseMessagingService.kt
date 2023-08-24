b9b0635d514e994c5dcb1896746addcd1b05d0fe
selcannarin <narinselcan@gmail.com>
2023-08-24 12:14:46 +0300

    add Firebase Messaging Service

---
+++ app/src/main/java/com/example/schoolbustrackerparent/ui/notification/MyFirebaseMessagingService.kt
@@ -0,0 +1,90 @@
+package com.example.schoolbustrackerparent.ui.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.schoolbustrackerparent.R
+import com.example.schoolbustrackerparent.ui.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ViewModelScoped
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.random.Random
+
+private const val CHANNEL_ID = "my_channel"
+
+@AndroidEntryPoint
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    @ViewModelScoped
+    lateinit var fcmViewModel: FCMViewModel
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        fcmViewModel.onTokenRefresh(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID = Random.nextInt()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val currentTime =
+            SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
+
+        val title = message.data["title"]
+        val body = message.data["message"]
+        val notificationMessage = "$body\n\nReceived at: $currentTime"
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(notificationMessage)
+            .setSmallIcon(R.drawable.bus_icon)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(notificationID, notification)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channelName = "channelName"
+        val channel = NotificationChannel(
+            CHANNEL_ID, channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = "My channel description"
+            enableLights(true)
+            lightColor = Color.GREEN
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+}
+
+
